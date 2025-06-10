bbd970f5a7b27f87f1c03f4db1e38c74d8aa5f72
surakaushik <surakaushik30@gmail.com>
2025-06-10 19:11:56 +0530

    new commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,14 @@
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
-  if (sidebar.style.width === "25%") {
+  if (sidebar.style.width === "20%") {
     sidebar.style.width = "0";
     document.getElementById("navbar").style.width = "100%"
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("btn").style.color = 'black';
   } else {
-    sidebar.style.width = "25%";
-    document.getElementById("navbar").style.width = "75%"
-    document.getElementById("main").style.marginLeft = "25%";
+    sidebar.style.width = "20%";
+    document.getElementById("navbar").style.width = "80%"
+    document.getElementById("main").style.marginLeft = "20%";
+    document.getElementById("btn").style.color = 'white';
   }
 }
-function loadPage(page) {
-  fetch(page)
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById('firstPage').innerHTML = html;
-    });
-}
-
-//  function handleSubmit(event) {
-//     event.preventDefault();
-//     const formData = new FormData(event.target);
-//     const entries = Object.fromEntries(formData.entries());
-
-//     // Display the form data (for demonstration)
-//     let output = '<h3>Submitted Party Details:</h3><ul>';
-//     for (let [key, value] of Object.entries(entries)) {
-//       output += `<li><strong>${key}:</strong> ${value}</li>`;
-//     }
-//     output += '</ul>';
-
-//     document.getElementById('main-content').innerHTML = output;
-//   }
